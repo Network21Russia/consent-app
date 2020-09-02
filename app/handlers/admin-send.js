f9c51fd07913ac71592e990212330cdb124ab30d
Evgeniy Strigo <james.kotoff@gmail.com>
2020-09-02 13:05:23 +0300

    Prevent multiply resend letter to the same customer

--- app/handlers/admin-send.js
+++ app/handlers/admin-send.js
@@ -96,7 +96,7 @@ module.exports = async (ctx, next) => {
 
         const totalPages = Math.ceil(totalCount / itemsOnPage);
 
-        finished = totalPages <= offset;
+        finished = hash ? true : totalPages <= offset;
     }
 
     const template = 'admin-send';
