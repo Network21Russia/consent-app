604154c53d2d67e890dc55b5f31e34a5018bd27c
Evgeniy Strigo <james.kotoff@gmail.com>
2020-09-03 23:33:28 +0300

    Consent template updated

--- app/utils/render-pdf.js
+++ app/utils/render-pdf.js
@@ -6,11 +6,18 @@ async function renderPdf(html) {
     const pdfOptions = {
         format: 'A4',
         orientation: "portrait",
-        border: '2cm',
+        border: '20mm',
         type: "pdf",
         dpi: 200,
         quality: 80,
         zoomFactor: "1",
+        paginationOffset: 1,
+        footer: {
+            height: "15mm",
+            contents: {
+                default: '<div style="text-align: center">страница {{page}} из {{pages}}</div>',
+            }
+        },
     };
     return new Promise((resolve, reject) => {
         pdf.create(html, pdfOptions).toBuffer(async function (err, buffer) {
